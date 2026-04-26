106744fa255dd548205812b1e68b3b9db337def6
Beauty Maurya <beautymaurya9142@gmail.com>
2026-04-26 22:17:28 +0530

    Struct

--- Basics/5.Struct.cpp
+++ Basics/5.Struct.cpp
@@ -1,93 +1,146 @@
-// //Struct: userdefined data type which allow us to group diff type of varibale under a single name:
+// // //Struct: userdefined data type which allow us to group diff type of varibale under a single name:
 
-// // struct name{
-// //     //body
-// // }
+// // // struct name{
+// // //     //body
+// // // }
 
-// #include <iostream>
-// #include <algorithm>
-// using namespace std;
+// // #include <iostream>
+// // #include <algorithm>
+// // using namespace std;
 
-// struct person{//struct(only blueprint no memory allocation)
-//     string name;
-//     int age;
-//     string gender;
-// };
+// // struct person{//struct(only blueprint no memory allocation)
+// //     string name;
+// //     int age;
+// //     string gender;
+// // };
+
+// // // int main(){
+// // //     person p1;//object(memory allocation)
+// // //     p1.name="Subh";
+// // //     p1.age=20;
+// // //     p1.gender="Male";
+// // //     cout<<p1.name<<" "<<p1.age<<" "<<p1.gender<<endl;
+// // // }
+
+// // //we can decalre struct in main in 3 way:
 
 // // int main(){
-// //     person p1;//object(memory allocation)
-// //     p1.name="Subh";
-// //     p1.age=20;
-// //     p1.gender="Male";
-// //     cout<<p1.name<<" "<<p1.age<<" "<<p1.gender<<endl;
+// //     person p1;
+// //     p1.name = "Subh";
+// //     p1.age = 20;
+// //     p1.gender = "Male";
+// //     cout << p1.name << " " << p1.age << " " << p1.gender << std::endl;
+
+// //     //way2: direct initialization
+// //     person p2{"Subh", 20, "Male"};
+// //     cout << p2.name << " " << p2.age << " " << p2.gender << std::endl;
+
+// //     //way:  Designated initialization (C++20)
+// //     person p3 {.name = "Subh", .age = 20, .gender = "Male"}; //order doesn't matter
+// //     cout << p3.name << " " << p3.age << " " << p3.gender << std::endl;
+
+// //     return 0;
 // // }
 
-// //we can decalre struct in main in 3 way:
+// // 2.struct in function:
+// #include <iostream>
+// #include <string>
+// using namespace std;
 
-// int main(){
-//     person p1;
-//     p1.name = "Subh";
-//     p1.age = 20;
-//     p1.gender = "Male";
-//     cout << p1.name << " " << p1.age << " " << p1.gender << std::endl;
+// struct student
+// {
+//     string name;
+//     int age;
+//     float marks;
+// };
 
-//     //way2: direct initialization
-//     person p2{"Subh", 20, "Male"};
-//     cout << p2.name << " " << p2.age << " " << p2.gender << std::endl;
+//memory: int 4 bytes,string: 8 bytes,float: 4 bytes and padding 4 bytes(padding means: )
 
-//     //way:  Designated initialization (C++20)
-//     person p3 {.name = "Subh", .age = 20, .gender = "Male"}; //order doesn't matter
-//     cout << p3.name << " " << p3.age << " " << p3.gender << std::endl;
+// void print(student &s)
+// {
+//     s.marks = 80;
+//     cout << s.marks << endl;
+// }
 
+// void updateval(student &s)
+// {
+//     s.marks = 100;
+//     cout << s.marks << endl;
+// }
+
+// // return struct
+// student createstudent(string name, int age, float marks)
+// {
+//     student s;
+//     s.name = name;
+//     s.age = age;
+//     s.marks = marks;
+//     return s;
+// };
+
+// int main()
+// {
+//     student s1 = {"Subh", 20, 90};
+//     print(s1); // only copy value pass by value
+//     cout << s1.marks << endl;
+//     updateval(s1); // orignal value change - pass by ref
+//     cout << s1.marks << endl;
+
+//     student s2 = createstudent("Subh", 20, 90);
+//     cout << s2.name << " " << s2.age << " " << s2.marks << endl;
 //     return 0;
 // }
 
-// 2.struct in function:
+// //class and struct- only diff is struct be default(public) and class by default(private)
+// //store in memory: size=sum of all data type size
+
+// class student{
+//     public:
+//     string name;
+//     int age;
+//     float marks;
+// };
+
+// //object
+// student s1;
+// student s2;//different memory allocation
+
+// function :
+//  class student{
+//      public:
+//      int marks;
+
+//     void display(){
+//         cout<<marks; //this pointer(address of current object)
+//     }
+// };
+
+// static and normal: memory allocation
+// static: every object share same memory
+// normal: every object have different(own) memory
+
 #include <iostream>
-#include <string>
 using namespace std;
 
-struct student
+class Student
 {
-    string name;
-    int age;
-    float marks;
+public:
+    int marks;
+    static int schoolcode;
 };
 
-void print(student &s)
-{
-    s.marks = 80;
-    cout << s.marks << endl;
+
+int Student::schoolcode=100;
+
+int main(){
+   Student s1,s2;
+   s1.marks=90;
+   s2.marks=89;
+   cout<<s1.marks<<" "<<s2.marks<<endl;
+   cout<<s1.schoolcode<<" "<<s2.schoolcode<<endl;
+
+   Student::schoolcode=200;
+   cout<<s1.schoolcode<<" "<<s2.schoolcode<<endl;
+   return 0;
 }
 
-void updateval(student &s)
-{
-    s.marks = 100;
-    cout << s.marks << endl;
-}
-
-// return struct
-student createstudent(string name, int age, float marks)
-{
-    student s;
-    s.name = name;
-    s.age = age;
-    s.marks = marks;
-    return s;
-};
-
-int main()
-{
-    student s1 = {"Subh", 20, 90};
-    print(s1); // only copy value pass by value
-    cout << s1.marks << endl;
-    updateval(s1); // orignal value change - pass by ref
-    cout << s1.marks << endl;
-
-    student s2 = createstudent("Subh", 20, 90);
-    cout << s2.name << " " << s2.age << " " << s2.marks << endl;
-    return 0;
-}
-
-
-//class and struct- only diff is struct be default(public) and class by default(private) 
---
+++ Basics/6.BasicOOPs.cpp
@@ -0,0 +1,15 @@
+//oops: combining properties and function into one unit called class.
+
+//class(custom data type) and object(memory allocation):
+//-userdefined data type which define properties and behavior of an object.(blueprint of object)
+//object- it is instance of that class which represent real world entity and occupy memory in the computer's memory.
+
+
+//4 pillars of oops:
+//1. Encapsulation: hide the internal details and provide a public interface to access them.
+//2. Abstraction: hide the internal details and provide a public interface to access them.
+//3. Inheritance: allow a class to inherit properties and behavior from another class.
+//4. Polymorphism: allow a class to have multiple forms
+
+
+
