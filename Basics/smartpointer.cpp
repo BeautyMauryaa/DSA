9ce4df8ad279566fb614246f48369c4fe5fdde1c
BeautyMauryaa <beautymaurya9142@gmail.com>
2026-05-12 23:54:00 +0530

    Smart Pointer pract

--- Basics/smartpointer.cpp
+++ Basics/smartpointer.cpp
@@ -1,2 +1,20 @@
 // a class that behaves like a pointer but automatically manages memory
 // it automatically destroy memory when no longer needed
+//we have three types of smart pointer:
+//1.unique_ptr(unique one owner only  )
+#include<iostream>
+#include<memory>
+using namespace std;
+
+void uni_ptr(){
+    unique_ptr<int> ptr=make_unique<int>(10);
+    cout<<*ptr;
+    cout<<*ptr;
+}
+
+int main(){
+    uni_ptr();
+    return 0;
+}
+//2.shared_ptr
+//3.weak_ptr
