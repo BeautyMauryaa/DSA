85fe988194d0bae0eaa07aa4323a5ff74c7898cc
BeautyMauryaa <beautymaurya9142@gmail.com>
2026-06-03 01:20:30 +0530

    palindrome leetcode sol

---
+++ leetcode/Palindrome09.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(int x)
+{
+    if (x < 0)
+        return false;
+    int temp = x;
+    long long rev = 0;
+    while (rev > 0)
+    {
+        int digit = x % 10;     // get the last digit
+        rev = rev * 10 + digit; // add the last digit to rev
+        x = x / 10;             // remove the last digit from x
+    }
+    return temp == rev;
+}
+
+int main()
+{
+    int x;
+    cin >> x;
+    cout << isPalindrome(x);
+    return 0;
+}
