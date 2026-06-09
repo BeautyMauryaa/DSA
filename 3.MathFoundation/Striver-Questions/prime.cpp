32beb1077d380cc892c7a938671540fda0baac75
BeautyMauryaa <beautymaurya9142@gmail.com>
2026-06-09 23:16:55 +0530

    solved

---
+++ 3.MathFoundation/Striver-Questions/prime.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int prime(int n){
+    if(n<=0){
+        return 0;
+    }
+
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<prime(n);
+}
