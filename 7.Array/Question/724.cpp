2db6320c1b9500ba6663aaa36949052c455e6b6d
BeautyMauryaa <beautymaurya9142@gmail.com>
2026-07-11 13:16:51 +0530

    pivot element

---
+++ 7.Array/Question/724.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> arr={1,7,3,6,5,6};
+    int n=arr.size();
+    vector<int> prefix(n);
+    prefix[0]=arr[0];
+    for(int i=1;i<n;i++){
+        prefix[i]=prefix[i-1]+arr[i];
+    }
+    
+    //find left sum  and right sum
+    for(int i=0;i<n;i++){
+        int leftsum;
+        if(i==0){
+            leftsum=0;
+        }
+        else{
+            leftsum=prefix[i-1];
+        }
+
+        int rightsum=prefix[n-1]-prefix[i];
+        if(leftsum==rightsum){
+            cout<<i<<"\n";
+        }
+    }
+    return 0;
+}
