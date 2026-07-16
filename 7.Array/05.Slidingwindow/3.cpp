b12c94bb3d1900b06cf93760cd6961756523a8ae
BeautyMauryaa <beautymaurya9142@gmail.com>
2026-07-16 23:39:26 +0530

    leetcode 3 problem

---
+++ 7.Array/05.Slidingwindow/3.cpp
@@ -0,0 +1,18 @@
+// maximum subarray
+class Solution{
+    public:
+    int lengthofLongestSubstring(string s){
+        unordered_set<char> ch;
+        int left=0;
+        int maxlen=0;
+        for(int i=0;i<s.length();i++{
+            while(ch.count(s[i])){
+                ch.erase(s[left]);
+                left++;
+            }
+            ch.insert(s[i]);
+            maxlen=max(maxlen,i-left+1);
+        }
+        return maxlen;
+    }
+}
